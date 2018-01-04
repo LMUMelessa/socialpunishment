31899a8581a27a11e5aa571900617640bd363f95
Patrick Betz <patrick.betz@awi.uni-heidelberg.de>
2018-01-04 15:18:22 +0100

    Implemented voting stage. Soon to merge.

--- pg_vote_famfeud/models.py
+++ pg_vote_famfeud/models.py
@@ -28,7 +28,7 @@ class Subsession(BaseSubsession):
         labellist = ['Player A', 'Player B', 'Player C', 'Player D', 'Player E']
         for group in self.get_group_matrix():
             for player in group:
-                player.label = labellist[player.id_in_group-1]
+                player.playerlabel = labellist[player.id_in_group-1]
 
     def creating_session(self):
         # Assign treatment
@@ -52,11 +52,20 @@ class Subsession(BaseSubsession):
 class Group(RedwoodGroup):
 
 
+    all_play = models.BooleanField(
+        doc= 'Boolean. Defines if all player play the social arena game in a round.')
+
+
     total_cont = models.CurrencyField(
         doc='The overall contribution of the group in the public good game.')
 
     indiv_share = models.CurrencyField(
         doc='The share the players get after the public good game.')
+
+    # Is needed so that we can display on any template the excluded player
+    excluded_player = models.CharField(
+        doc='The player, who is excluded from the social game.')
+
 
     # Payoffs for pg game; function also sets total_cont and indiv_share
     def set_payoffs(self):
@@ -66,7 +75,61 @@ class Group(RedwoodGroup):
             p.payoff = (Constants.endowment - p.contribution) + self.indiv_share
 
 
+    # Assigns for all the players how many votes they had
+    # The variable updated is player.myvotes
+    def set_myvotes(self):
+        for set_player in self.get_players():
+            vote_count = 0
+            # Screen the invitations of all players
+            for voter in self.get_players():
+                votesdic = {'Player A': voter.vote_A,
+                            'Player B': voter.vote_B,
+                            'Player C': voter.vote_C,
+                            'Player D': voter.vote_D,
+                            'Player E': voter.vote_E}
+                # Check if the voter did vote for the set_player
+                if votesdic[set_player.playerlabel] == True:
+                    vote_count += 1
+            set_player.myvotes = vote_count
 
+
+
+    # Find out if there is a majority after the voting
+    # Assign for all players if they play in the second game (social game, familyfeud)
+    # Assign if the second game/social game/family feud will be played with all players in the group
+    # Sets group.all_play and player.plays
+    def set_social_game(self):
+        invitationslist = []
+
+        # Collect all the votes
+        for player in self.get_players():
+            invitationslist.append(player.myvotes)
+        # Find the highest number of votes a player had
+        _max = max(invitationslist)
+
+        # Check if max is unique, then there is a majority
+        occurence = invitationslist.count(_max)
+        # Unique max, there is a majority
+        if occurence == 1:
+            self.all_play = False
+            # Identify the player with the most exclusions
+            for player in self.get_players():
+                if player.myvotes == _max:
+                    # The default is True
+                    player.plays = False
+                    # Break, as only one player has max votes because max is unique
+                    break
+        # Not unique, no majority
+        elif occurence > 1:
+            self.all_play = True
+
+
+    def set_excluded_player(self):
+        for player in self.get_players():
+            if player.plays == False:
+                self.excluded_player = player.playerlabel
+                # Break, because only one player is excluded
+                break
 
 
 class Player(BasePlayer):
@@ -89,6 +152,33 @@ class Player(BasePlayer):
         verbose_name='What do you want to contribute to the project?',
         min=0,
         max=Constants.endowment)
+
+
+    myvotes = models.IntegerField(
+        doc='The number of votes the player got after the public good game.')
+
+    plays = models.BooleanField(
+        doc='Determines if the player is allowed to play the social game.',
+        default = True
+    )
+
+
+    # Variables where player can vote to exclude one player from the social arena game
+    vote_A = models.BooleanField(
+        widget=widgets.CheckboxInput(),
+        verbose_name='Player A')
+    vote_B = models.BooleanField(
+        widget=widgets.CheckboxInput(),
+        verbose_name='Player B')
+    vote_C = models.BooleanField(
+        widget=widgets.CheckboxInput(),
+        verbose_name='Player C')
+    vote_D = models.BooleanField(
+        widget=widgets.CheckboxInput(),
+        verbose_name='Player D')
+    vote_E = models.BooleanField(
+        widget=widgets.CheckboxInput(),
+        verbose_name='Player E')
 
 
 
--- pg_vote_famfeud/views.py
+++ pg_vote_famfeud/views.py
@@ -20,13 +20,60 @@ class ResultsPG(Page):
         data = {}
         #TODO: Note: Vars for template cannot be tested, therefore this has to be safe and doublechecked
         for player in self.group.get_players():
-            #remove whitespace from label so that it can be displayed in the template
-            data[(player.playerlabel).replace(' ','')] = player.cont
-            #also display the profit the player made
+            # Remove whitespace from label so that it can be displayed in the template
+            data[(player.playerlabel).replace(' ','')] = player.contribution
+            # Also display the profit the player made
             data['gameprofit'] = Constants.endowment - self.player.contribution + self.group.indiv_share
         return data
 
 
+class Vote(Page):
+    # Only needed in the voting treatment
+    def is_displayed(self):
+        return self.player.treatment == 'voting'
+
+    form_model = models.Player
+    form_fields = ['vote_A', 'vote_B','vote_C','vote_D','vote_E']
+
+    #TODO: Be cautious, if the initial values of the vote_X are changed
+    def error_message(self, values):
+        vote_count = sum([values['vote_A'], values['vote_B'], values['vote_C'], values['vote_D'],values['vote_E']])
+        if vote_count > 1:
+            return 'You can only vote for one player, who you want to exclude.'
+
+    def vars_for_template(self):
+        data = {}
+        # TODO: Note Vars for template cannot be tested, therefore this has to be safe and doublechecked
+        for player in self.group.get_players():
+            # Remove whitespace from label so that it can be displayed in the template
+            data[(player.playerlabel).replace(' ', '')] = player.contribution
+        return data
+
+
+class VoteWaitPage(WaitPage):
+    # Only needed in voting treatment
+    def is_displayed(self):
+        return self.player.treatment == 'voting'
+
+    def after_all_players_arrive(self):
+        # Count the votes for every player
+        self.group.set_myvotes()
+        # Assign for each player if he plays the social game
+        self.group.set_social_game()
+        # Set the excluded player label on a group variable, if there is one
+        self.group.set_excluded_player()
+
+
+class VoteResults(Page):
+   # Only needed in voting treatment
+   def is_displayed(self):
+       return self.player.treatment == 'voting'
+   def vars_for_template(self):
+       data = {}
+       for player in self.group.get_players():
+           data[(player.playerlabel).replace(' ', '') + '_votes'] = player.myvotes
+           data[(player.playerlabel).replace(' ', '') + '_plays'] = player.plays
+       return data
 
 
 
@@ -39,5 +86,8 @@ page_sequence = [
     Instructions,
     Contribution,
     FirstWaitPage,
-    ResultsPG
+    ResultsPG,
+    Vote,
+    VoteWaitPage,
+    VoteResults
 ]
